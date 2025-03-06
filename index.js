e7f1c041f5e2bef7a586e2bda9646ce9235ef730
Jose <josefbautista94@gmail.com>
2025-03-05 23:52:47 -0500

    Finished the sum of numbers section for part 1

---
+++ index.js
@@ -0,0 +1,30 @@
+//Part 1: Thinking Functionally
+
+// While there is rarely a “correct” answer in programming, it is important to keep 
+// your audience (other programmers) in mind.
+//  Write functions with descriptive names, and clear inputs and outputs.
+// With that in mind, write functions that accomplish the following:
+// Take an array of numbers and return the sum.
+// Take an array of numbers and return the average.
+// Take an array of strings and return the longest string.
+// Take an array of strings, and a number and return an array of the 
+// strings that are longer than the given number. 
+// For example, 
+// stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"].
+// Take a number, n, and print every number 
+// between 1 and n without using loops. Use recursion.
+
+
+// Take an array of numbers and return the sum.
+let arrOfNums = [32, 43, 67, 35, 23, 12]
+function sumOfNumbers(numArr) {
+
+   let sum = 0;
+    for (let nums of numArr) {
+        sum += nums
+    }
+
+    return sum;
+}
+
+console.log(sumOfNumbers(arrOfNums))
