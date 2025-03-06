8500ede652d1073dc99cdd250b6cd622783d18db
Jose <josefbautista94@gmail.com>
2025-03-06 02:03:03 -0500

    completed the recursion part of the assignment, soo completed part 1 of the assignment

--- index.js
+++ index.js
@@ -17,6 +17,7 @@
 let arrOfNums = [32, 43, 67, 35, 23, 12, 43, 72, 6, 256, 512];
 let arrayOfStrings = ["random", "array", "of", "strings", "haha", "French Bulldogs", "pizza", "Hennesy Wings"];
 let arrOfStrings2 = ['say', 'hello', 'in', 'the', 'morning']
+
 // Take an array of numbers and return the sum.
 function sumOfNumbers(numArr) {
 
@@ -57,11 +58,11 @@ console.log(`This is the longest string in the array: ${returnLongestString(arra
 // Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
 // For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"].
 
-function stringsLongerThan(stringArr,num){
+function stringsLongerThan(stringArr, num) {
     let newArr = []
 
-    for(let i = 0; i < stringArr.length; i++){
-        if(stringArr[i].length > num){
+    for (let i = 0; i < stringArr.length; i++) {
+        if (stringArr[i].length > num) {
             newArr.push(stringArr[i])
         }
     }
@@ -69,4 +70,29 @@ function stringsLongerThan(stringArr,num){
 
 }
 
-console.log(stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3));
+console.log(stringsLongerThan(arrOfStrings2, 3));
+
+//Take a number, n, and print every number between 1 and n without using loops. Use recursion.
+
+//I needed help for this part so here are my resources because I didn't quite understand in the beggining!
+/*
+ * Function to print numbers from 'start' to 'end' using recursion.
+ * Adapted from: "JavaScript Program to Print 1 to N using Recursion"
+ * Author: GeeksforGeeks
+ * URL: https://www.geeksforgeeks.org/javascript-program-to-print-1-to-n-using-recursion/
+ * Retrieved on: 03/06/2025
+ */
+
+let recursionNumber = 30;
+
+function recursion(num,n) { // num is the number where start, and current number being printed and n is the end goal
+  
+    if (num > n) { // checking if the current number goes over the end goal if it does the if statement stops by returning
+       return;
+    }
+    console.log(num) // we're priting where we currently are at
+  recursion(num + 1, n); // recursion takes place here, we're add 1 to num till the if statement becomes true, num becomes 31 it it terminates the function.
+}
+
+console.log(recursion(1,recursionNumber))
+
