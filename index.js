edb7d57f732b26db7104cace71834caad3cc3fcf
Jose <josefbautista94@gmail.com>
2025-03-06 11:35:53 -0500

    finished the first problem of part2

--- index.js
+++ index.js
@@ -85,15 +85,34 @@ console.log(stringsLongerThan(arrOfStrings2, 3));
 
 let recursionNumber = 30;
 
-function recursion(num,n) { // num is the number where start, and current number being printed and n is the end goal
-  
+function recursion(num, n) { // num is the number where start, and current number being printed and n is the end goal
+
     if (num > n) { // checking if the current number goes over the end goal if it does the if statement stops by returning
-       return;
+        return;
     }
     console.log(num) // we're printing where we currently are at
-  recursion(num + 1, n); // recursion takes place here, we're adding 1 to num till the if statement becomes true, if num becomes 31 terminates the function.
+    recursion(num + 1, n); // recursion takes place here, we're adding 1 to num till the if statement becomes true, if num becomes 31 terminates the function.
 }
 
-console.log(recursion(1,recursionNumber))
+console.log(recursion(1, recursionNumber))
 
 // Part 2: Thinking Methodically
+
+let personArr = [{ id: "42", name: "Bruce", occupation: "Knight", age: "41" }, { id: "48", name: "Barry", occupation: "Runner", age: "25" }, { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" }, { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" }, { id: "7", name: "Bilbo", occupation: "None", age: "111" }]
+//learning how to use map real quck
+
+//using map to find the ages and converting them to numbers
+let ages = personArr.map(person => Number(person.age))
+console.log(ages)
+let names = personArr.map(person => (person.name))
+console.log(names)
+
+
+// Sort the array by age.
+
+function sortByAge(byAge) {
+    let sortedAges = byAge.sort((a, b) => Number(a.age) - Number(b.age)); // turning the string numbers into int's and then using .sort() to put them in order.
+    return sortedAges;
+}
+
+console.log(sortByAge(personArr))
